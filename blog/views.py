933d1f44bd2ca96797a03ca2db0e61612bc79560
cniblock <nibbo1984@hotmail.co.uk>
2024-04-30 14:09:02 +0000

    fixed indentation error on blog views post

--- blog/views.py
+++ blog/views.py
@@ -32,15 +32,15 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
-    comment_form = CommentForm(data=request.POST)
-    if comment_form.is_valid():
-        comment = comment_form.save(commit=False)
-        comment.author = request.user
-        comment.post = post
-        comment.save()
-        messages.add_message(
-            request, messages.SUCCESS,
-            'Comment submitted and awaiting approval'
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
     )
 
     comment_form = CommentForm()
